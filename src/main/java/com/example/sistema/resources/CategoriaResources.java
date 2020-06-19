d9701771d82aeae4dc05d57b5584068f2d3c3f1b
jvkass <jvcnmata@gmail.com>
2020-06-19 10:41:26 -0300

    'TestantoPrimeiraClasseDeDominio'

--- src/main/java/com/example/sistema/resources/CategoriaResources.java
+++ src/main/java/com/example/sistema/resources/CategoriaResources.java
@@ -1,5 +1,10 @@
 package com.example.sistema.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.sistema.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+
+        Categoria cat = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
