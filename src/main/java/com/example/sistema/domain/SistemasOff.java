bf86fcfe83b8ea134b287117b3d19838dc7317c0
jvkass <jvcnmata@gmail.com>
2020-06-22 08:36:54 -0300

    'ConexaoBancoPostgre'

---
+++ src/main/java/com/example/sistema/domain/SistemasOff.java
@@ -0,0 +1,121 @@
+package com.example.sistema.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sistemaoff")
+public class SistemasOff implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private String nome;
+    private Date dataInicio;
+    private Date dataFim;
+    private String descricao;
+    private Integer codigoAd;
+
+    public SistemasOff() {
+
+    }
+
+    public SistemasOff(Integer id, String nome, Date dataInicio, Date dataFim, String descricao, Integer codigoAd) {
+        this.id = id;
+        this.nome = nome;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.descricao = descricao;
+        this.codigoAd = codigoAd;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigoAd() {
+        return codigoAd;
+    }
+
+    public void setCodigoAd(Integer codigoAd) {
+        this.codigoAd = codigoAd;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SistemasOff other = (SistemasOff) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+   
+
+    
+    
+}
