884d9d61f5fd63edb7f0a2a6d3fcb8fabe25dd4d
jvkass <jvcnmata@gmail.com>
2020-06-23 12:01:23 -0300

    'PutDelete'

--- src/main/java/com/example/sistema/resources/SistemaOffResource.java
+++ src/main/java/com/example/sistema/resources/SistemaOffResource.java
@@ -6,9 +6,11 @@ import com.example.sistema.domain.SistemaOff;
 import com.example.sistema.repository.SistemaOffRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,22 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class SistemaOffResource {
 
-@Autowired
-SistemaOffRepository sistemaOffRepository;
+    @Autowired
+    SistemaOffRepository sistemaOffRepository;
 
-@GetMapping("/sistemaoff")
-public List<SistemaOff> listaSistemas(){
-    return sistemaOffRepository.findAll();
-}
+    @GetMapping("/sistemaoff")
+    public List<SistemaOff> listaSistemas() {
+        return sistemaOffRepository.findAll();
+    }
 
-@GetMapping("/sistemaoff/{id}")
-public SistemaOff listaSistemaUnico(@PathVariable(value = "id") Integer id) {
-    return sistemaOffRepository.findById(id).orElseThrow(null);
-}
+    @GetMapping("/sistemaoff/{id}")
+    public SistemaOff listaSistemaUnico(@PathVariable(value = "id") Integer id) {
+        return sistemaOffRepository.findById(id).orElseThrow(null);
+    }
 
-@PostMapping("/sistemaoff")
-public SistemaOff salvaSistemaOff(@RequestBody SistemaOff sistemaOff){
-    return sistemaOffRepository.save(sistemaOff);
-}
-    
+    @PostMapping("/sistemaoff")
+    public SistemaOff salvaSistemaOff(@RequestBody SistemaOff sistemaOff) {
+        return sistemaOffRepository.save(sistemaOff);
+    }
+
+    @DeleteMapping("/sistemaoff")
+    public void deletaSistemaOff(@RequestBody SistemaOff sistemaOff) {
+        sistemaOffRepository.delete(sistemaOff);
+    }
+
+    @PutMapping("/sistemaoff")
+    public SistemaOff atualizaSistemaOff(@RequestBody SistemaOff sistemaOff) {
+        return sistemaOffRepository.save(sistemaOff);
+    }
+
 }
